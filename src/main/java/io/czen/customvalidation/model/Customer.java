745217307140f9203cf48790fbb7f035187d997c
czen0002 <zc114789@gmail.com>
2022-09-14 23:20:28 +1000

    [CHENG] add Customer

---
+++ src/main/java/io/czen/customvalidation/model/Customer.java
@@ -0,0 +1,33 @@
+package io.czen.customvalidation.model;
+
+import io.czen.customvalidation.util.Club;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class Customer {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Size(max = 5, message = "MobileCountryCode length must not be greater than 5")
+    @Pattern(regexp = "|\\+[0-9]+")
+    private String mobileCountryCode;
+
+    @Size(max = 30, message = "MobileNumber length must not be greater than 30")
+    @Pattern(regexp = "|[0-9]+")
+    private String mobileNumber;
+
+    @Size(max = 100, message = "EmailAddress length must not be greater than 100")
+    @Email(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*" +
+            "@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$",
+            message = "EmailAddress must be a well-formed email address")
+    private String emailAddress;
+
+    private Club club;
+}
