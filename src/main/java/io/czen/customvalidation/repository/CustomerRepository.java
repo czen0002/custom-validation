8d89cd5d6e56f5181a76e1ab778c8323c5060311
czen0002 <zc114789@gmail.com>
2022-09-15 23:32:13 +1000

    [CHENG] remove unnecessary entity and repository

--- src/main/java/io/czen/customvalidation/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package io.czen.customvalidation.repository;
-
-import io.czen.customvalidation.entity.CustomerEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Long, CustomerEntity> {
-}
