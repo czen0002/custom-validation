f16a27e64067b1fc9ad0229da8c2ef11fbaee0bd
czen0002 <zc114789@gmail.com>
2022-09-16 22:32:32 +1000

    [CHENG] replace Club with String in ClubValidator

--- src/main/java/io/czen/customvalidation/annotation/ClubValidator.java
+++ src/main/java/io/czen/customvalidation/annotation/ClubValidator.java
@@ -1,17 +1,15 @@
 package io.czen.customvalidation.annotation;
 
-import io.czen.customvalidation.util.Club;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
 import java.util.List;
 
-public class ClubValidator implements ConstraintValidator<ClubValidation, Club> {
+public class ClubValidator implements ConstraintValidator<ClubValidation, String> {
 
     @Override
-    public boolean isValid(Club value, ConstraintValidatorContext context) {
-        List<Club> list = Arrays.asList(Club.RED, Club.SILVER, Club.GOLD);
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        List<String> list = Arrays.asList("RED", "SILVER", "GOLD");
         return list.contains(value);
     }
 }
